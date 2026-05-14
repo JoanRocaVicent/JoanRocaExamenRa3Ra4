6878dccab0527f54982a44a578986925ed0b3bcf
Joan Roca <joanroca@inspladelestany.cat>
2026-05-14 18:32:44 +0200

    commit 1

---
+++ src/test/java/cat/inspla/ra3/reserves/ServeiReservesOptimitzacioTest.java
@@ -0,0 +1,20 @@
+package cat.inspla.ra3.reserves;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServeiReservesOptimitzacioTest {
+
+    @Test
+    void TODO_recursosOrdenatsPerNomRetornaCopiaOrdenada() {
+        // TODO: afegeix recursos en ordre desordenat i comprova que el mètode retorna noms ordenats.
+        // Important: el mètode no ha de modificar l'ordre original de getRecursos().
+    }
+
+    @Test
+    void TODO_generarInformeInclouNomTipusIEstat() {
+        // TODO: crea un servei amb recursos disponibles i reservats.
+        // Comprova que l'informe conté nom, tipus i estat de cada recurs.
+    }
+}
---
+++ src/test/java/cat/inspla/ra3/reserves/ServeiReservesTest.java
@@ -0,0 +1,13 @@
+package cat.inspla.ra3.reserves;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServeiReservesTest {
+
+    @Test
+    void TODO_afegirRecursIncrementaLlista() {
+        // TODO: afegeix un recurs i comprova la mida de la llista.
+    }
+}
